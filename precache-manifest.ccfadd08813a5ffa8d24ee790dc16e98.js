33f0985b020fa004cf776a094193522d982b592f
TRaadityan <tv.aadit@gmail.com>
2020-08-05 19:57:51 +0530

    Updates

---
+++ precache-manifest.ccfadd08813a5ffa8d24ee790dc16e98.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d12a16d59b0510419a13dcface3ed0e6",
+    "url": "/trapdate/index.html"
+  },
+  {
+    "revision": "c06ab396b4b18356c8b9",
+    "url": "/trapdate/static/css/main.fa212d5b.chunk.css"
+  },
+  {
+    "revision": "7eba40a5bcf300ef9861",
+    "url": "/trapdate/static/js/2.0e871f02.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trapdate/static/js/2.0e871f02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c06ab396b4b18356c8b9",
+    "url": "/trapdate/static/js/main.1351957c.chunk.js"
+  },
+  {
+    "revision": "450abf2debcea9559ae4",
+    "url": "/trapdate/static/js/runtime-main.879c4135.js"
+  },
+  {
+    "revision": "51358e27f3c26207e58f0abe5f3c65fc",
+    "url": "/trapdate/static/media/BTTF.51358e27.ttf"
+  },
+  {
+    "revision": "3e01f9c9a8b87096cbce6ad3629b4c73",
+    "url": "/trapdate/static/media/IMG_20181226_122904.3e01f9c9.jpg"
+  }
+]);
